2a1b504e06a289610a71beb08c9f7485c6b371ae
Frad LEE <fradser@gmail.com>
2022-09-16 14:45:23 +0800

    build(xcode): initialize project

---
+++ Shared/TwoWayCommunicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayCommunicationApp.swift
+//  Shared
+//
+//  Created by Frad LEE on 2022/9/16.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayCommunicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
